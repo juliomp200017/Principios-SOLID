3f81143b3de6dce1f435209cd2ff4451733ccce3
juliomp200017 <juliomp200017@gmail.com>
2022-11-21 01:17:41 -0500

    fix

--- Models/Juego.cs
+++ Models/Juego.cs
@@ -1,7 +1,8 @@
 ﻿namespace Solid.Models
 {
     public interface Juego
-    {
+    {   
+        string Name { get; set; }
         public string Id { get; set; }
 
         public decimal Price { get; set; }
@@ -10,13 +11,54 @@
 
         public decimal getprice();
     }
-    public interface Adventure : Juego
+    public interface Juegodatos
+    {
+
+        public decimal getInvoice();
+        public string getId();
+        public string getName();
+
+    }
+    public class Adventure : Juego,Juegodatos
     {
         public string Id { get; set; }
 
         public string Name { get; set; }
 
         public decimal Invoice { get; set; }
+
+        public decimal Price { get ; set; }
+
+        public string getId()
+        {
+            return this.Id;
+        }
+
+        public decimal getInvoice()
+        {
+            return this.Invoice;
+        }
+
+        public string getName()
+        {
+            return this.Name;
+        }
+
+        public decimal getprice()
+        {
+            return Price * Invoice;
+        }
+    }
+
+    public class Accion : Juego
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Invoice { get; set; }
+        public decimal Price { get; set; }
+
         public decimal getprice()
         {
             return Price * Invoice;
--- Models/Persona.cs
+++ Models/Persona.cs
@@ -2,11 +2,26 @@
 {
     public abstract class Persona
     {
+        protected int id;
+        protected string nombre;
+        protected int edad;
         public abstract string getname();
     }
 
-    public class cliente : Persona
+    public abstract class PersonaConAdministracion : Persona
     {
+        protected string Correo;
+        public abstract string getcorreo();
+    }
+
+    public class cliente : Persona
+    {   
+        public cliente(int id, string nombre,int edad)
+        {
+            this.id = id;
+            this.nombre = nombre;
+            this.edad = edad;
+        }
         public override string getname()
         {
             return "cliente";
@@ -14,12 +29,24 @@
         }
     }
 
-    public class administrador : Persona
+    public class administrador : PersonaConAdministracion
     {
+        public administrador(int id, string nombre, int edad,string correo)
+        {
+            this.Correo = correo;
+            this.id = id;
+            this.nombre = nombre;
+            this.edad = edad;
+        }                                                                               
         public override string getname()
         {
             return "administrador";
 
+        }
+
+        public override string getcorreo()
+        {
+             return this.Correo;
         }
     }
 }
